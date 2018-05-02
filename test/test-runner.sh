df72b95212ea92f4556c9163fa8b1fd47110c728
Andrew Faulkner <andfaulkner@gmail.com>
2018-05-02 00:46:28 -0400

    Change test file format: test/VAR_NAME_value.test.js
    Test file format changed, and runner also upgraded accordingly. Filename starts with the name of the variable (as its CLI name e.g. NODE_ENV) in all caps, then is followed by the value it's set to in lowercase

--- test/test-runner.sh
+++ test/test-runner.sh
@@ -2,10 +2,9 @@
 
 ./node_modules/.bin/mocha ./test/default-env.test.js
 
-NODE_ENV=production ./node_modules/.bin/mocha ./test/NODE_ENV_eql_production.test.js
+NODE_ENV=production ./node_modules/.bin/mocha ./test/NODE_ENV_production.test.js
 
-LOG_LEVEL=silly ./node_modules/.bin/mocha ./test/LOG_LEVEL_eql_silly.test.js
+LOG_LEVEL=silly ./node_modules/.bin/mocha ./test/LOG_LEVEL_silly.test.js
+LOG_LEVEL=verbose ./node_modules/.bin/mocha ./test/LOG_LEVEL_verbose.test.js
 
-LOG_LEVEL=verbose ./node_modules/.bin/mocha ./test/LOG_LEVEL_eql_verbose.test.js
-
-SECURITY_TEST=true ./node_modules/.bin/mocha ./test/SECURITY_TEST_eql_true.test.js
+SECURITY_TEST=true ./node_modules/.bin/mocha ./test/SECURITY_TEST_true.test.js
